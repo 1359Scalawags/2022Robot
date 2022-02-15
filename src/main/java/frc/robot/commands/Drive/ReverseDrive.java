802b49f0fe351c32c644652098bf8ab4c9b10f6d
CedAnimator <ced601ced@gmail.com>
2022-02-14 18:32:00 -0800

    added reverse drive

--- src/main/java/frc/robot/commands/Drive/ReverseDrive.java
+++ src/main/java/frc/robot/commands/Drive/ReverseDrive.java
@@ -24,7 +24,9 @@ public class ReverseDrive extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        m_driveSystem.reverseDirection();
     }
+    
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
@@ -39,7 +41,7 @@ public class ReverseDrive extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 
     @Override
