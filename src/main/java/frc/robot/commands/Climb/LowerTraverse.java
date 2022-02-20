995838e072f50ed7a0ae1c006c2449752782d63c
Natashaxcode <antonich085@lebanon.k12.or.us>
2022-02-20 15:06:17 -0800

    Got Rid of Traverse Code

--- src/main/java/frc/robot/commands/Climb/LowerTraverse.java
+++ src/main/java/frc/robot/commands/Climb/LowerTraverse.java
@@ -1,42 +1,42 @@
-package frc.robot.commands.Climb;
+// package frc.robot.commands.Climb;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ClimbSystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// import frc.robot.subsystems.ClimbSystem;
 
-/**
- *
- */
-public class LowerTraverse extends CommandBase {
-    private ClimbSystem m_climbSystem;
+// /**
+//  *
+//  */
+// public class LowerTraverse extends CommandBase {
+//     private ClimbSystem m_climbSystem;
 
-    public LowerTraverse(ClimbSystem subsystem) {
-        m_climbSystem = subsystem;
-        addRequirements(m_climbSystem);  
-    }
+//     public LowerTraverse(ClimbSystem subsystem) {
+//         m_climbSystem = subsystem;
+//         addRequirements(m_climbSystem);  
+//     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-    }
+//     // Called when the command is initially scheduled.
+//     @Override
+//     public void initialize() {
+//     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-    }
+//     // Called every time the scheduler runs while the command is scheduled.
+//     @Override
+//     public void execute() {
+//     }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
-    }
+//     // Called once the command ends or is interrupted.
+//     @Override
+//     public void end(boolean interrupted) {
+//     }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+//     // Returns true when the command should end.
+//     @Override
+//     public boolean isFinished() {
+//         return false;
+//     }
 
-    @Override
-    public boolean runsWhenDisabled() {
-        return false;
-    }
-}
+//     @Override
+//     public boolean runsWhenDisabled() {
+//         return false;
+//     }
+// }
