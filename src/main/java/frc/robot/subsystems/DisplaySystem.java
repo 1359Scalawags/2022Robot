3b05e7b46ce06c684b26cae4986c9c3254322612
CedAnimator <ced601ced@gmail.com>
2022-03-06 14:27:58 -0800

    Allowed on the fly checkin of voltage

--- src/main/java/frc/robot/subsystems/DisplaySystem.java
+++ src/main/java/frc/robot/subsystems/DisplaySystem.java
@@ -4,23 +4,34 @@ import java.util.Map;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DriverStation;
+
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+
+
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.Climb;
 
+
+
 public class DisplaySystem extends SubsystemBase {
+    PowerDistribution m_pdh = new PowerDistribution(Constants.DisplaySystem.PDHCANID,ModuleType.kRev);
 
     private ShuffleboardTab mainTab = Shuffleboard.getTab("Match Tab");
     private NetworkTableEntry timeEntry;
     private NetworkTableEntry climbLockEntry;
+    private NetworkTableEntry batteryVolege;
     private ClimbSystem climbSystem;
 
     public DisplaySystem(ClimbSystem climber) {
         climbSystem = climber;
         Shuffleboard.selectTab("Match Tab");
-        timeEntry = mainTab
+            timeEntry = mainTab
                         .add("Match Time", 0)
                         .withWidget(BuiltInWidgets.kDial)
                         .withProperties(Map.of("min", 0, "max", 135))
@@ -28,12 +39,20 @@ public class DisplaySystem extends SubsystemBase {
                         .withPosition(10, 0)
                         .getEntry();
 
-        climbLockEntry = mainTab
+            climbLockEntry = mainTab
                         .add("Climber Status", true)
                         .withWidget(BuiltInWidgets.kBooleanBox)
                         .withSize(3,1)
                         .withProperties(Map.of("Color when true", "green", "Color when false", "maroon"))
                         .withPosition(0, 0)
+                        .getEntry();
+
+            batteryVolege = mainTab
+                        .add("Batter Voltege", 0)
+                        .withWidget(BuiltInWidgets.kNumberBar)
+                        .withProperties(Map.of("min", 0, "max", 135))
+                        .withSize(1,3)
+                        .withPosition(4, 0)
                         .getEntry();
     }
 
@@ -45,6 +64,9 @@ public class DisplaySystem extends SubsystemBase {
         }
         if(climbLockEntry != null) {
             climbLockEntry.setBoolean(!climbSystem.getClimberLocked());
+        }
+        if(batteryVolege != null) {
+            batteryVolege.setDouble(m_pdh.getVoltage());
         }
     }
 
