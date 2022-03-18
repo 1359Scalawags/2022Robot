11e586bdf54ee6c6532c2a2b34561573cda214a3
mrhelland <tim.helland@lebanon.k12.or.us>
2022-03-18 11:42:35 -0700

    Implement PID tuner on Shuffleboard.

--- src/main/java/frc/robot/helper/PIDPositionTuner.java
+++ src/main/java/frc/robot/helper/PIDPositionTuner.java
@@ -14,15 +14,11 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Map;
 
-public class PIDVelocityTuner {
-
-    private String name;
-
+public class PIDPositionTuner {
     private SparkMaxPIDController controller;
     private RelativeEncoder encoder;
 
     private ShuffleboardTab tunerTab;
-    // dedicated entries for each value
     private NetworkTableEntry pEntry;
     private NetworkTableEntry iEntry;
     private NetworkTableEntry dEntry;
@@ -37,12 +33,13 @@ public class PIDVelocityTuner {
     private double errorValue;
     private double actualValue;
 
-    public PIDVelocityTuner(String name, RelativeEncoder encoder, SparkMaxPIDController controller, PIDValues initialValues, int initialTarget) {
-        this.name = name;
+    public PIDPositionTuner(String name, RelativeEncoder encoder, SparkMaxPIDController controller, PIDValues initialValues, int initialTarget) {
         this.controller = controller;
         this.encoder = encoder;
         this.pidValues = new PIDValues(initialValues);
         this.targetValue = initialTarget;
+        tunerTab = Shuffleboard.getTab(name);
+        layoutTabs(tunerTab);
 
         controller.setP(this.pidValues.kP);
         controller.setI(this.pidValues.kI);
@@ -50,26 +47,51 @@ public class PIDVelocityTuner {
         controller.setIZone(this.pidValues.kIz);
         controller.setFF(this.pidValues.kFf);
 
-        initialize();
-
-    }
-
-
-
-    // ShuffleBoard version
-    private void initialize() {
-        tunerTab = Shuffleboard.getTab(name);
-        layoutShuffleboard(tunerTab);
         pEntry.setDouble(this.pidValues.kP);
         iEntry.setDouble(this.pidValues.kI);
         dEntry.setDouble(this.pidValues.kD);
         izEntry.setDouble(this.pidValues.kIz);
         ffEntry.setDouble(this.pidValues.kFf);
         targetEntry.setDouble(this.targetValue);
+
+
+        // SmartDashboard.putNumber("P Gain", pidValues.kP);
+        // SmartDashboard.putNumber("I Gain", pidValues.kI);
+        // SmartDashboard.putNumber("D Gain", pidValues.kD);
+        // SmartDashboard.putNumber("I Zone", pidValues.kIz);
+        // SmartDashboard.putNumber("Feed Forward", pidValues.kFf);
+        // SmartDashboard.putNumber("Target RPM", targetValue);
+        // actualValue = (int)encoder.getVelocity();
+        // errorValue = targetValue - actualValue;
+        // SmartDashboard.putNumber("Actual RPM", actualValue);
+        // SmartDashboard.putNumber("Error RPM", errorValue);
     }
 
-    // ShuffleBoard version
     public void periodic() {
+        syncEntries();
+        // double p = SmartDashboard.getNumber("P Gain", 0);
+        // double i = SmartDashboard.getNumber("I Gain", 0);
+        // double d = SmartDashboard.getNumber("D Gain", 0);
+        // double iz = SmartDashboard.getNumber("I Zone", 0);
+        // double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        // double target = SmartDashboard.getNumber("Target RPM", 0);
+
+        // if((p != pidValues.kP)) { controller.setP(p); pidValues.kP = p; }
+        // if((i != pidValues.kI)) { controller.setI(i); pidValues.kI = i; }
+        // if((d != pidValues.kD)) { controller.setD(d); pidValues.kD = d; }
+        // if((iz != pidValues.kIz)) { controller.setIZone(iz); pidValues.kIz = iz; }
+        // if((ff != pidValues.kFf)) { controller.setFF(ff); pidValues.kFf = ff; }
+        // if(target != targetValue) {controller.setReference(target, ControlType.kVelocity); targetValue = target;}
+
+        // actualValue = encoder.getVelocity();
+        // errorValue = targetValue - actualValue;
+        // SmartDashboard.putNumber("Actual RPM", actualValue);
+        // SmartDashboard.putNumber("Error RPM", errorValue);
+        // System.out.println("Target: " + targetValue + "  Actual: " + actualValue + "  Error: " + errorValue);
+
+    }
+
+    private void syncEntries() {
         double p = pEntry.getDouble(0);
         double i = iEntry.getDouble(0);
         double d = dEntry.getDouble(0);
@@ -92,7 +114,7 @@ public class PIDVelocityTuner {
         System.out.println("Actual: " + actualValue + " Error: " + errorValue);
     }
 
-    private void layoutShuffleboard(ShuffleboardTab shuffleTab) {
+    private void layoutTabs(ShuffleboardTab shuffleTab) {
         pEntry = shuffleTab
                     .add("P Gain", 0)
                     .withWidget(BuiltInWidgets.kTextView)
@@ -149,42 +171,5 @@ public class PIDVelocityTuner {
                     .withPosition(3, 1)
                     .getEntry();                    
     }
-
-    // SmartDashboard version
-    // private void initialize() {
-        // SmartDashboard.putNumber("P Gain", pidValues.kP);
-        // SmartDashboard.putNumber("I Gain", pidValues.kI);
-        // SmartDashboard.putNumber("D Gain", pidValues.kD);
-        // SmartDashboard.putNumber("I Zone", pidValues.kIz);
-        // SmartDashboard.putNumber("Feed Forward", pidValues.kFf);
-        // SmartDashboard.putNumber("Target RPM", targetValue);
-        // actualValue = (int)encoder.getVelocity();
-        // errorValue = targetValue - actualValue;
-        // SmartDashboard.putNumber("Actual RPM", actualValue);
-        // SmartDashboard.putNumber("Error RPM", errorValue);
-    // }
-
-    // SmartDashboard version
-    // private void periodic() {
-        // double p = SmartDashboard.getNumber("P Gain", 0);
-        // double i = SmartDashboard.getNumber("I Gain", 0);
-        // double d = SmartDashboard.getNumber("D Gain", 0);
-        // double iz = SmartDashboard.getNumber("I Zone", 0);
-        // double ff = SmartDashboard.getNumber("Feed Forward", 0);
-        // double target = SmartDashboard.getNumber("Target RPM", 0);
-
-        // if((p != pidValues.kP)) { controller.setP(p); pidValues.kP = p; }
-        // if((i != pidValues.kI)) { controller.setI(i); pidValues.kI = i; }
-        // if((d != pidValues.kD)) { controller.setD(d); pidValues.kD = d; }
-        // if((iz != pidValues.kIz)) { controller.setIZone(iz); pidValues.kIz = iz; }
-        // if((ff != pidValues.kFf)) { controller.setFF(ff); pidValues.kFf = ff; }
-        // if(target != targetValue) {controller.setReference(target, ControlType.kVelocity); targetValue = target;}
-
-        // actualValue = encoder.getVelocity();
-        // errorValue = targetValue - actualValue;
-        // SmartDashboard.putNumber("Actual RPM", actualValue);
-        // SmartDashboard.putNumber("Error RPM", errorValue);
-        // System.out.println("Target: " + targetValue + "  Actual: " + actualValue + "  Error: " + errorValue);
-    // }
 
 }
