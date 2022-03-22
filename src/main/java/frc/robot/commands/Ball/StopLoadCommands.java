7ada52822e89622e149406f86f6118a6dc02fa66
CedAnimator <ced601ced@gmail.com>
2022-03-21 17:50:34 -0700

    Added Arm commands

---
+++ src/main/java/frc/robot/commands/Ball/StopLoadCommands.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.Ball;
+
+import frc.robot.subsystems.BallHandlingSystem;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+// import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ *
+ */
+public class StopLoadCommands extends ParallelCommandGroup {
+
+        private BallHandlingSystem m_ballHandlingSystem;
+        // private boolean isBallAlreadyLoaded;
+        // private boolean isBallAlreadyStaged;
+
+    public StopLoadCommands(BallHandlingSystem subsystem) {
+        m_ballHandlingSystem = subsystem;
+        
+        addCommands(
+            new RaiseArm(subsystem),
+            new StopSpinMotor(subsystem)
+           // new LoadBall(subsystem)
+        );
+    }
+}
