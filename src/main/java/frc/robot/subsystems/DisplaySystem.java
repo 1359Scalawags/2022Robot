94428a8ffd69699d5f711ff2cbf2d107e5f21635
Mr Helland <tim.helland@lebanon.k12.or.us>
2022-03-24 13:02:36 -0700

    Add vision system to display.

--- src/main/java/frc/robot/subsystems/DisplaySystem.java
+++ src/main/java/frc/robot/subsystems/DisplaySystem.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 
 
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -23,34 +24,41 @@ public class DisplaySystem extends SubsystemBase {
     private NetworkTableEntry timeEntry;
     private NetworkTableEntry climbLockEntry;
     private NetworkTableEntry batteryVolege;
+    private ComplexWidget cameraView;
     private ClimbSystem climbSystem;
 
-    public DisplaySystem(ClimbSystem climber) {
+    public DisplaySystem(ClimbSystem climber, VisionSystem vision) {
         climbSystem = climber;
         Shuffleboard.selectTab("Match Tab");
-            timeEntry = mainTab
-                        .add("Match Time", 0)
-                        .withWidget(BuiltInWidgets.kDial)
-                        .withProperties(Map.of("min", 0, "max", 135))
-                        .withSize(3,3)
-                        .withPosition(7, 0)
-                        .getEntry();
+        timeEntry = mainTab
+                    .add("Match Time", 0)
+                    .withWidget(BuiltInWidgets.kDial)
+                    .withProperties(Map.of("min", 0, "max", 135))
+                    .withSize(3,3)
+                    .withPosition(7, 0)
+                    .getEntry();
 
-            climbLockEntry = mainTab
-                        .add("Climber Status", true)
-                        .withWidget(BuiltInWidgets.kBooleanBox)
-                        .withSize(3,1)
-                        .withProperties(Map.of("Color when true", "green", "Color when false", "maroon"))
-                        .withPosition(0, 0)
-                        .getEntry();
+        climbLockEntry = mainTab
+                    .add("Climber Status", true)
+                    .withWidget(BuiltInWidgets.kBooleanBox)
+                    .withSize(3,1)
+                    .withProperties(Map.of("Color when true", "green", "Color when false", "maroon"))
+                    .withPosition(0, 0)
+                    .getEntry();
 
-            batteryVolege = mainTab
-                        .add("Battery Voltege", 0)
-                        .withWidget(BuiltInWidgets.kNumberBar)
-                        .withProperties(Map.of("min", 0, "max", 135))
-                        .withSize(3,1)
-                        .withPosition(0, 1)
-                        .getEntry();
+        batteryVolege = mainTab
+                    .add("Battery Voltege", 0)
+                    .withWidget(BuiltInWidgets.kNumberBar)
+                    .withProperties(Map.of("min", 0, "max", 135))
+                    .withSize(3,1)
+                    .withPosition(0, 1)
+                    .getEntry();
+
+        cameraView = mainTab
+                    .add("Ball Camera", vision.getCamera1())
+                    .withWidget(BuiltInWidgets.kCameraStream)
+                    .withSize(5,4)
+                    .withPosition(3, 0);
     }
 
 
