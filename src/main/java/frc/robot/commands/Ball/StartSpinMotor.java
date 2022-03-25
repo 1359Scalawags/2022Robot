b67152704e3caf2f31e08beac5bf1b171ea5d98c
mrhelland <tim.helland@lebanon.k12.or.us>
2022-03-24 18:27:03 -0700

    Comment out unused code.

--- src/main/java/frc/robot/commands/Ball/StartSpinMotor.java
+++ src/main/java/frc/robot/commands/Ball/StartSpinMotor.java
@@ -1,53 +1,53 @@
 
-package frc.robot.commands.Ball;
+// package frc.robot.commands.Ball;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-//import edu.wpi.first.wpilibj2.command.Subsystem;
-import frc.robot.Constants;
-import frc.robot.subsystems.BallHandlingSystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// //import edu.wpi.first.wpilibj2.command.Subsystem;
+// import frc.robot.Constants;
+// import frc.robot.subsystems.BallHandlingSystem;
 
-/**
- *
- */
-@Deprecated
-public class StartSpinMotor extends CommandBase {
+// /**
+//  *
+//  */
+// @Deprecated
+// public class StartSpinMotor extends CommandBase {
 
-    private BallHandlingSystem m_ballHandlingSystem;
+//     private BallHandlingSystem m_ballHandlingSystem;
 
-    public StartSpinMotor(BallHandlingSystem subsystem) {
+//     public StartSpinMotor(BallHandlingSystem subsystem) {
 
-        m_ballHandlingSystem = subsystem;
-        //addRequirements(m_ballHandlingSystem);
+//         m_ballHandlingSystem = subsystem;
+//         //addRequirements(m_ballHandlingSystem);
 
-    }
+//     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-          m_ballHandlingSystem.setSpinMotor(Constants.BallHandling.kArmSpinMotorSpeed);
-    }
+//     // Called when the command is initially scheduled.
+//     @Override
+//     public void initialize() {
+//           m_ballHandlingSystem.setSpinMotor(Constants.BallHandling.kArmSpinMotorSpeed);
+//     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
+//     // Called every time the scheduler runs while the command is scheduled.
+//     @Override
+//     public void execute() {
 
 
-    }
+//     }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
+//     // Called once the command ends or is interrupted.
+//     @Override
+//     public void end(boolean interrupted) {
     
-    }
+//     }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() { 
-        return true;
-    }
+//     // Returns true when the command should end.
+//     @Override
+//     public boolean isFinished() { 
+//         return true;
+//     }
 
-    @Override
-    public boolean runsWhenDisabled() {
-        return false;
-    }
-}
+//     @Override
+//     public boolean runsWhenDisabled() {
+//         return false;
+//     }
+// }
--- src/main/java/frc/robot/commands/Ball/StopSpinMotor.java
+++ src/main/java/frc/robot/commands/Ball/StopSpinMotor.java
@@ -1,53 +1,53 @@
 
-package frc.robot.commands.Ball;
+// package frc.robot.commands.Ball;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-// import frc.robot.Constants;
-import frc.robot.subsystems.BallHandlingSystem;
+// import edu.wpi.first.wpilibj2.command.CommandBase;
+// // import frc.robot.Constants;
+// import frc.robot.subsystems.BallHandlingSystem;
 
-/**
- *
- */
-public class StopSpinMotor extends CommandBase {
+// /**
+//  *
+//  */
+// public class StopSpinMotor extends CommandBase {
 
-    private BallHandlingSystem m_ballHandlingSystem;
+//     private BallHandlingSystem m_ballHandlingSystem;
 
-    public StopSpinMotor(BallHandlingSystem subsystem) {
+//     public StopSpinMotor(BallHandlingSystem subsystem) {
 
-        m_ballHandlingSystem = subsystem;
-        //addRequirements(m_ballHandlingSystem);
+//         m_ballHandlingSystem = subsystem;
+//         //addRequirements(m_ballHandlingSystem);
 
-    }
+//     }
 
-    // Called when the command is initially scheduled.
-    @Override
-    public void initialize() {
-        m_ballHandlingSystem.setSpinMotor(0);
+//     // Called when the command is initially scheduled.
+//     @Override
+//     public void initialize() {
+//         m_ballHandlingSystem.setSpinMotor(0);
   
-    }
+//     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
+//     // Called every time the scheduler runs while the command is scheduled.
+//     @Override
+//     public void execute() {
      
 
-    }
+//     }
 
-    // Called once the command ends or is interrupted.
-    @Override
-    public void end(boolean interrupted) {
+//     // Called once the command ends or is interrupted.
+//     @Override
+//     public void end(boolean interrupted) {
     
-    }
+//     }
 
-    // Returns true when the command should end.
-    @Override
-    public boolean isFinished() {
-        return true;
+//     // Returns true when the command should end.
+//     @Override
+//     public boolean isFinished() {
+//         return true;
       
-    }
+//     }
 
-    @Override
-    public boolean runsWhenDisabled() {
-        return false;
-    }
-}
+//     @Override
+//     public boolean runsWhenDisabled() {
+//         return false;
+//     }
+// }
