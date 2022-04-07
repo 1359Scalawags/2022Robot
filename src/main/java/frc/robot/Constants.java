4c3919cc4ed3f1515b9ded762f2871c8cdcad86d
WildstrikePlayz <Jasmer1702@lebanon.k12.or.us>
2022-04-07 15:34:44 -0700

    adjusted speed and boost speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,8 +22,8 @@ public class Constants {
         public static final int kRightFrontPort = 11;
         public static final int kRightRearPort = 12;
         //Drive motor speeds
-        public static final float kTeleopBoostSpeed = 0.55f;
-        public static final float kTeleopBaseDriveSpeed = 0.45f;        
+        public static final float kTeleopBoostSpeed = 0.25f;
+        public static final float kTeleopBaseDriveSpeed = 0.5f;        
         public static final float kDriveSpeed = 0.85f;
 
         //Motor varibles
