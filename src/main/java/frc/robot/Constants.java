ca1a0dff926d53f8bf7dd4788f3edf625693b49a
CedAnimator <ced601ced@gmail.com>
2022-09-14 18:49:44 -0700

    Reverted Teleop Drive speeds to original values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
         public static final int kRightRearPort = 12;
         //Drive motor speeds
         public static final float kTeleopBoostSpeed = 0.25f;
+        //boost was 0.25
         public static final float kTeleopBaseDriveSpeed = 0.5f;        
         public static final float kDriveSpeed = 0.85f;
 
